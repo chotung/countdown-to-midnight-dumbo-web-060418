b9be70a83788254bf89a81380d50c23fd39a7fe6
Choeles Tung <choelestung@gmail.com>
2018-06-01 16:25:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -14,6 +14,16 @@ def countdown_with_sleep(int)
 end
 
 
-evenArray = Array.new(5) + 1
 
-while evenArray 
+def printArray
+  int = 0
+  evenArray = Array.new(5) + 1
+    while evenArray.length > int do
+      if evenArray[int] % 2 == 0
+        puts evenArray[int]
+          else
+          puts "not even"
+      end
+      int += 1
+    end
+  end
